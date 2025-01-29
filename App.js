4e217e769957e4afe4cc80294b5379ff399193d7
mounikasadineni01 <mounisadineni04@gmail.com>
2025-01-28 19:35:54 -0600

    react-episode1

---
+++ App.js
@@ -0,0 +1,25 @@
+
+const parent = React.createElement(
+    "div",
+    {id:"parent"},
+    [React.createElement(
+        "div",
+        {id:"child1"},
+        [React.createElement("h1",{id:"heading"},"Hello h1"),
+        React.createElement("h2",{id:"heading2"},"Hello h2")]
+    ),
+    React.createElement(
+        "div",
+        {id:"child2"},
+        [React.createElement("h1",{id:"heading"},"Hello h1"),
+        React.createElement("h2",{id:"heading2"},"Hello h2")]
+    )]
+);
+
+/* const heading = React.createElement(
+    "h1",
+    {id:"heading"},
+    "Hello world"
+); */
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
---
+++ Sample.js
@@ -0,0 +1,18 @@
+/* const arr = [
+    {firstname: "Mounika", secondname:"sadineni", age:23},
+    {firstname: "Hari", secondname:"Karra", age:22},
+    {firstname: "Deepu", secondname:"Man", age:35},
+    {firstname: "Saera", secondname:"Dis", age:47}
+];
+
+const fname = arr.reduce(function (acc,curr){
+
+    if(curr.age>30)
+    {
+        acc.push(curr.firstname);
+    }
+    return acc;
+},[]);
+console.log(fname); */
+
+
